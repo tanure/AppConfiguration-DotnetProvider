40f5c9af719af86b925b46afc8fc0c365c49b725
Avani Gupta <avanigupta@users.noreply.github.com>
2022-04-06 11:50:37 -0700

    Initial changes

--- src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationRefresherProvider.cs
+++ src/Microsoft.Extensions.Configuration.AzureAppConfiguration/AzureAppConfigurationRefresherProvider.cs
@@ -8,38 +8,50 @@ using System.Linq;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration
 {
-    internal class AzureAppConfigurationRefresherProvider : IConfigurationRefresherProvider
-    {
-        public IEnumerable<IConfigurationRefresher> Refreshers { get; }
+	internal class AzureAppConfigurationRefresherProvider : IConfigurationRefresherProvider
+	{
+		public IEnumerable<IConfigurationRefresher> Refreshers { get; }
 
-        public AzureAppConfigurationRefresherProvider(IConfiguration configuration, ILoggerFactory _loggerFactory)
-        {
-            var configurationRoot = configuration as IConfigurationRoot;
-            var refreshers = new List<IConfigurationRefresher>();
+		public AzureAppConfigurationRefresherProvider(IConfiguration configuration, ILoggerFactory _loggerFactory)
+		{
+			var refreshers = new List<IConfigurationRefresher>();
 
-            if (configurationRoot != null)
-            {
-                foreach (IConfigurationProvider provider in configurationRoot.Providers)
-                {
-                    if (provider is IConfigurationRefresher refresher)
-                    {
-                        // Use _loggerFactory only if LoggerFactory hasn't been set in AzureAppConfigurationOptions
-                        if (refresher.LoggerFactory == null)
-                        {
-                            refresher.LoggerFactory = _loggerFactory;
-                        }
+			if (configuration != null)
+			{
+				AddRefreshers(configuration, refreshers, _loggerFactory);
+			}
 
-                        refreshers.Add(refresher);
-                    }
-                }
-            }
+			if (!refreshers.Any())
+			{
+				throw new InvalidOperationException("Unable to access the Azure App Configuration provider. Please ensure that it has been configured correctly.");
+			}
 
-            if (!refreshers.Any())
-            {
-                throw new InvalidOperationException("Unable to access the Azure App Configuration provider. Please ensure that it has been configured correctly.");
-            }
+			Refreshers = refreshers;
+		}
 
-            Refreshers = refreshers;
-        }
-    }
+		private void AddRefreshers(IConfiguration configuration, List<IConfigurationRefresher> refreshers, ILoggerFactory _loggerFactory)
+		{
+			var configurationRoot = configuration as IConfigurationRoot;
+			if (configurationRoot != null)
+			{
+				foreach (IConfigurationProvider provider in configurationRoot.Providers)
+				{
+					if (provider is IConfigurationRefresher refresher)
+					{
+						// Use _loggerFactory only if LoggerFactory hasn't been set in AzureAppConfigurationOptions
+						if (refresher.LoggerFactory == null)
+						{
+							refresher.LoggerFactory = _loggerFactory;
+						}
+
+						refreshers.Add(refresher);
+					}
+					else if (provider is ChainedConfigurationProvider chainedProvider)
+					{
+						AddRefreshers(chainedProvider.Configuration, refreshers);
+					}
+				}
+			}
+		}
+	}
 }
